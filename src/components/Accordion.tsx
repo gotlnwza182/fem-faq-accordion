6acabe2342339783e0a00c2dcf081e6e7a011ef4
gotlnwza182 <6131501073@lamdaun.mfu.ac.th>
2025-09-10 18:51:27 +0700

    add focus outline for keyboard only users

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -39,7 +39,7 @@ const Accordion: React.FC<AccordionProps> = ({
         aria-controls={`accordion-${id}-panel`}
         onClick={onToggle}
         onKeyDown={handleKeyDown}
-        className="flex items-center justify-between w-full cursor-pointer"
+        className="flex items-center justify-between w-full cursor-pointer focus:outline-2 focus:outline-violet-600 focus:outline-offset-2"
       >
         <span className="text-preset-2 w-11/12  text-start">{question}</span>
         <span>
